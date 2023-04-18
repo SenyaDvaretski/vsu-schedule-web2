bd4ec3cdf5fc48af8bc0858f852f8422a38a0803
poramental <poramental@mail.ru>
2023-04-18 22:22:50 +0300

    add Thymeleaf to project.

--- src/main/java/com/vsuscheduleweb/Controllers/AdminController.java
+++ src/main/java/com/vsuscheduleweb/Controllers/AdminController.java
@@ -2,11 +2,21 @@ package com.vsuscheduleweb.Controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@RequestMapping("vsuAdmin/*")
 @Controller
 public class AdminController {
+
+
+    @RequestMapping(value = "vsuAdmin/**", method = RequestMethod.GET)
+    public String getAdminPage(){
+        return "index";
+    }
+
+
 
 }
---
+++ src/main/java/com/vsuscheduleweb/Controllers/AdminRestController.java
@@ -0,0 +1,17 @@
+package com.vsuscheduleweb.Controllers;
+
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminRestController {
+
+
+    // TODO method will uploading schedule from admin page
+    public ResponseEntity uploadSchedule(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
