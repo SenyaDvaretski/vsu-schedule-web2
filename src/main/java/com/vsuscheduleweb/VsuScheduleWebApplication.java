fd492d5c84a0c09b53bb3cfa65f1789bff40d822
user <arseny.dvoretsky@innowise-group.com>
2023-04-18 19:18:23 +0300

    Initial commit

---
+++ src/main/java/com/vsuscheduleweb/VsuScheduleWebApplication.java
@@ -0,0 +1,13 @@
+package com.vsuscheduleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsuScheduleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsuScheduleWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsuscheduleweb/VsuScheduleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsuscheduleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsuScheduleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
